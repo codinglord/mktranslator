af4791b1a08f9592221fac90e1b8f79e0de5829a
mcpyx.info@gmail.com <mcpyx.info@gmail.com@74ca3961-c396-e74e-6f60-dfa2277785ac>
2011-11-26 15:14:19 +0000

    J-Vocaburary

---
+++ branches/CodeEngine.MK/CodeEngine.MK/Views/Trainings/J1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CodeEngine.MK.Views.Trainings
+{
+    partial class J1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.cmbSelector = new System.Windows.Forms.ComboBox();
+            this.mainMenu2 = new System.Windows.Forms.MainMenu();
+            this.lblSelector = new System.Windows.Forms.Label();
+            this.mainMenu4 = new System.Windows.Forms.MainMenu();
+            this.mainMenu3 = new System.Windows.Forms.MainMenu();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblHead = new System.Windows.Forms.Label();
+            this.mainMenu5 = new System.Windows.Forms.MainMenu();
+            this.SuspendLayout();
+            // 
+            // cmbSelector
+            // 
+            this.cmbSelector.Location = new System.Drawing.Point(4, 52);
+            this.cmbSelector.Name = "cmbSelector";
+            this.cmbSelector.Size = new System.Drawing.Size(233, 22);
+            this.cmbSelector.TabIndex = 30;
+            this.cmbSelector.SelectedIndexChanged += new System.EventHandler(this.cmbSelector_SelectedIndexChanged);
+            // 
+            // lblSelector
+            // 
+            this.lblSelector.Location = new System.Drawing.Point(4, 29);
+            this.lblSelector.Name = "lblSelector";
+            this.lblSelector.Size = new System.Drawing.Size(100, 20);
+            this.lblSelector.Text = "lblSelector";
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(4, 100);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtQuestion.Size = new System.Drawing.Size(233, 75);
+            this.txtQuestion.TabIndex = 32;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Location = new System.Drawing.Point(4, 77);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(135, 20);
+            this.lblQuestion.Text = "lblQuestion";
+            // 
+            // lblHead
+            // 
+            this.lblHead.Location = new System.Drawing.Point(3, 9);
+            this.lblHead.Name = "lblHead";
+            this.lblHead.Size = new System.Drawing.Size(100, 20);
+            this.lblHead.Text = "lblHead";
+            // 
+            // J1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.cmbSelector);
+            this.Controls.Add(this.lblSelector);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.lblHead);
+            this.Menu = this.mainMenu1;
+            this.Name = "J1";
+            this.Text = "J1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSelector;
+        private System.Windows.Forms.MainMenu mainMenu2;
+        private System.Windows.Forms.Label lblSelector;
+        private System.Windows.Forms.MainMenu mainMenu4;
+        private System.Windows.Forms.MainMenu mainMenu3;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblHead;
+        private System.Windows.Forms.MainMenu mainMenu5;
+    }
+}
